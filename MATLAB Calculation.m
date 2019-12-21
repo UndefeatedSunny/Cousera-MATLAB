bb4709d3ebafc03a0c3c7b79c463f4971f8424fa
Sunny Chaudhary <51516961+UndefeatedSunny@users.noreply.github.com>
2019-12-21 23:01:22 +0530

    Update MATLAB Calculation.m

--- MATLAB Calculation.m
+++ MATLAB Calculation.m
@@ -1,13 +1,11 @@
-%                          MATLAB as a Calculator
+                       MATLAB as a Calculator
 
-% We borrowed $1000 at a 10% annual interest rate. If we did not make a payment for two years, and assuming there is no penalty for non-payment, how much do we owe now? Assign the result to a variable called debt.
+We borrowed $1000 at a 10% annual interest rate. If we did not make a payment for two years, and assuming there is no penalty for non-payment, how much do we owe now? Assign the result to a variable called debt.
 
 
-
-%           Script
+CODE ->
 
 p = 1000; %Principle
 r = 10/100;   %Rate of Interest
 t = 2;    %Time in Years
 debt = (p)*((1+r)^t)   % Compound Interest
-
